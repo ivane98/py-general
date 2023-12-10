87795cc4e85a55517a7b361cf4d1dbe1d6b4aa39
ivane98 <i.vardoshvili22@gmail.com>
2023-12-10 11:59:22 +0400

    m

--- war.py
+++ war.py
@@ -1,25 +1,6 @@
-def is_prime(n):
-    for i in range(2, int(n**.5 + 1)):
-        if n % i == 0:
-            return False
-    return True
-
-def gap(g, m, n):
-    start = 0
-    end = 0
-    for i in range(m,n):
-      if is_prime(i):
-        if start == 0:
-          start = i
-        elif end == 0:
-          end = i
-        else:
-          start = end
-          end = i
-      if end - start == g:
-        return [start, end]
-    return None
-          
+def find_next_square(sq):
+    # Return the next square if sq is a square, -1 otherwise
+    return round(sq ** 0.5, 0)
 
 
-print(gap(6,100,110))
+print(find_next_square(121))
