b7ed42c0e93b2e75dad53c54ac25f2684a325832
Ivane Vardoshvili <ivardoshvili@moh.local>
2023-12-11 11:12:50 +0400

    m

--- war.py
+++ war.py
@@ -1,25 +1,2 @@
-def is_prime(n):
-    for i in range(2, int(n**.5 + 1)):
-        if n % i == 0:
-            return False
-    return True
-
-def gap(g, m, n):
-    start = 0
-    end = 0
-    for i in range(m,n):
-      if is_prime(i):
-        if start == 0:
-          start = i
-        elif end == 0:
-          end = i
-        else:
-          start = end
-          end = i
-      if end - start == g:
-        return [start, end]
-    return None
-          
-
-
-print(gap(6,100,110))
+def spin_words(sentence):
+    return " ".join([w[::-1] if len(w) >= 5 else w for w in sentence.split()])
