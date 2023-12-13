f75970e8e495fd66bd5bee8aada5c4fc7d606ce1
ivane98 <i.vardoshvili22@gmail.com>
2023-12-13 11:57:43 +0400

    1312

--- oop/knn/main.py
+++ oop/knn/main.py
@@ -1,15 +1,17 @@
-from typing import Optional
+from typing import Optional, List
+import weakref
+import datetime
+from collections.abc import Iterable
 
 
 class Sample:
-
     def __init__(self, sepal_length: float, sepal_width: float, petal_length: float, petal_width: float, species: Optional[str] = None) -> None:
         self.sepal_length = sepal_length
         self.sepal_width = sepal_width
         self.petal_length = petal_length
         self.petal_width = petal_width
         self.species = species
-        self.collections = Optional[str] = None
+        self.classification: Optional[str] = None
 
     def __repr__(self) -> str:
         if self.species is None:
@@ -19,7 +21,7 @@ class Sample:
         if self.classification is None:
             classification = ""
         else:
-            classification = f", {self.classification}"
+            classification = f"{self.classification}"
 
         return (
             f"{known_unknown}("
@@ -27,10 +29,65 @@ class Sample:
             f"sepal_width={self.sepal_width}, "
             f"petal_length={self.petal_length}, "
             f"petal_width={self.petal_width}, "
-            f"species={self.species!r}"
-            f"{classification}"
+            f"species={self.species!r}, "
+            f"classification={classification}"
             f")"
         )
+    
+    def classify(self, classification: str) -> None:
+        self.classification = classification
+
+    def matches(self)-> bool:
+        return self.species == self.classification
+    
+s2 = Sample(sepal_length=5.1, sepal_width=3.5, petal_length=1.4, petal_width=0.2, species="Iris-setosa")
+s2.classification = "Wrong"
+
+
+class Hyperparameter:
+    """A hyperparameter value and the overall quality of the
+classification."""
+
+    def __init__(self, k: int, training: "TrainingData") -> None:
+        self.k = k
+        self.data: weakref.ReferenceType["TrainingData"] = weakref.ref(training)
+        self.quality: float
+
+    def test(self):
+        """Run the entire test suite."""
+        training_data: Optional["TrainingData"] = self.data()
+
+        if not training_data:
+            raise RuntimeError("Broken Weak Reference")
+
+        pass_count, fail_count = 0, 0
+        for sample in training_data.testing:
+            sample.classification = self.classify(sample)
+        
+        if self.matches():
+            pass_count += 1
+        else:
+            fail_count += 1
+
+        self.quality = pass_count / (pass_count + fail_count)
+
+
+class TrainingData:
+    """A set of training data and testing data with methods to load and
+test the samples."""
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.uploaded: datetime.datetime
+        self.tested: datetime.datetime
+        self.training: List[Sample] = []
+        self.testing: List[Sample] = []
+        self.tuning: List[Hyperparameter] = []
+
+    
+
+
+
     
     
 
--- war.py
+++ war.py
@@ -1,9 +1,1 @@
-def find_it(seq):
 
-    for i in seq:
-        if seq.count(i) % 2 != 0:
-            return i
-
-
-
-print(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]))
