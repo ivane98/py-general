1509841b79524e18e095f7749d819918b30f872f
ivane98 <i.vardoshvili22@gmail.com>
2024-01-11 15:52:01 +0400

    110101

--- py-course/flask_app/weather.py
+++ py-course/flask_app/weather.py
@@ -5,7 +5,22 @@ from pprint import pprint
 
 load_dotenv()
 
+
+
 def get_current_weather(city='Tbilisi'):
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv('API_KEY')}&q={city}&units=metric'
 
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == "__main__":
+    print("\nGet current weather conditions")
+
+    city = input('\nPlease enter a city name')
+
+    weather_data = get_current_weather(city)
+
+    print('\n')
+    pprint(weather_data)
     
