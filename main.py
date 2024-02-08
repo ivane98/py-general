5c5a8d0f5e807feaa5fd4a26df4c834609d2334e
ivane98 <i.vardoshvili22@gmail.com>
2024-02-08 09:38:24 +0400

    0802

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import datetime
-import pytz
+import csv
 
-dt = datetime.datetime(2016, 7, 24, 12, 30, 45, tzinfo=pytz.UTC)
-dt_now = datetime.datetime.now()
-print(dt_now)
+html_output = ''
+names = []
+
+with open('patrons.csv', 'r') as csv_file:
+    csv_data = csv.DictReader(csv_file)
+
+    next(csv_data)
+
+    for l in csv_data:
+        if l['FirstName'] == 'No Reward':
+            break
+        names.append(f"{l['FirstName']} {l['LastName']}")
+
+
+html_output += f"<p>There are {len(names)} contributors</p>"
+
+print(html_output)
 
 
 
