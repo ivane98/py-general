fb1a539d1dc002a6f0a9d6d5898bf25c2839e3bf
ivane98 <i.vardoshvili22@gmail.com>
2024-02-12 10:17:14 +0400

    1202

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import requests
 
-payload = {'username': 'corey', "password": 'testing'}
+class Employee:
+    """A sample Employee class"""
 
-r = requests.get('https://httpbin.org/basic-auth/corey/testing', auth=('corey', 'testing'))
+    raise_amt = 1.05
 
-print(r)
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
 
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
 
 
