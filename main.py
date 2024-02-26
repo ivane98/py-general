14cabcc96d3c1cc035576ef0db148f0e0d7f10d8
ivane98 <i.vardoshvili22@gmail.com>
2024-02-26 10:59:27 +0400

    2602

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+import json
 
-nordic_countries = names(0, 4)
+def sort_dict_by_value(d, reverse=False):
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
+ 
+def most_spoken_languages(fname, value):
+    f = open(fname, encoding="UTF8")
+    to_analyse = json.load(f)
 
-print(nordic_countries)
+    total_languages_initial = []
+    counts = {}
+    output_list = []
+
+    for i in to_analyse:
+        total_languages_initial.extend(i["languages"])
+
+    for i in total_languages_initial:
+        counts[i] = counts.get(i, 0) + 1
+
+    counts = sort_dict_by_value(counts, True)
+
+    for i in list(counts.items())[:value]:
+        output_list.append(i)
+
+    f.close()
+
+    return [(sub[1], sub[0]) for sub in output_list]
+
+
+
+
+print(most_spoken_languages('countries_data.json', 10))
+
+
 
